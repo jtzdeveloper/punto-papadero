ffbeac16403de612fd0fda747e7c7e941cbaedc5
Johnny Torres <jony.tz@gmail.com>
2023-01-16 19:29:54 -0300

    commit 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import './App.css';
 
 
-import Nav from './components/Nav';
-import { Routes, Route,Link } from 'react-router-dom';
+/* import Nav from './components/Nav'; */
+/* import { Routes, Route,Link } from 'react-router-dom';
 import Screen from './components/Screen'
 import ScreenCategories from './components/ScreenCategories'
 import ScreenNewOrder from './components/ScreenNewOrder'
-import ScreenProductsByCategory from './components/ScreenProductsByCategory'
+import ScreenProductsByCategory from './components/ScreenProductsByCategory' */
 import { useState } from 'react';
 function App() {
   const [takeOrder, setTakeOrder] = useState({ line_items:[] })
-  const takesOrders = { takeOrder,setTakeOrder}
   return (
    <div className="bg-gray-900 min-h-screen flex items-start justify-center">
       <div className="bg-gray-800 h-min flex-1 flex flex-col lg:flex-row space-y-5 lg:space-y-0 lg:space-x-5  max-w-6xl sm:p-6 sm:my-2 sm:mx-4 sm:rounded-2xl">
-        <Nav />
-        <Screen>
+        {/* <Nav /> */}
+        {/* <Screen>
           <Routes>
             <Route path='/' element={ <ScreenCategories /> }></Route>
             <Route path='newOrder' element={ <ScreenNewOrder /> }> </Route>
-            <Route path='productsByCategory/:id_category' element={<ScreenProductsByCategory takeOrder={takeOrder} setTakeOrder={setTakeOrder} />}></Route>
+            <Route path='productsByCategory/:id_category' element={<ScreenProductsByCategory takeOrder={takeOrder} setTakeOrder={setTakeOrder} />}></Route> 
           </Routes>
-        </Screen>
+        </Screen>  */}
       </div>
   </div>
   )
