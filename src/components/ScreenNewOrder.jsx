9c3ef17597fe9420ac2a0f3336d0c9f6cd5470f2
Johnny Torres <jony.tz@gmail.com>
2023-01-18 23:33:26 -0300

    commit

--- src/components/ScreenNewOrder.jsx
+++ src/components/ScreenNewOrder.jsx
@@ -21,7 +21,7 @@ export default function ScreenNewOrder({ takeOrder }){
                   </a>
                 </div>
               </div>
-              <section className="w-full h-full flex items-center flex-col xl:flex-row gap-1    ">
+              <section className="w-full h-[calc(100vh-150px)] flex items-center flex-col xl:flex-row gap-1    ">
                 
                 <section className="bg-slate-900 h-[calc(100vh-500px)] sm:h-[calc(100vh-400px)]   overflow-y-auto w-full  xl:w-1/2  rounded-xl text-sm mt-10 border-separate text-gray-400 ">
                     {
