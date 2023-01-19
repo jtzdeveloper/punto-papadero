dacea094dff3604e1776af15a0d01c22b8994f9c
Johnny Torres <jony.tz@gmail.com>
2023-01-19 10:03:10 -0300

    commit

--- src/components/ScreenNewOrder.jsx
+++ src/components/ScreenNewOrder.jsx
@@ -21,7 +21,7 @@ export default function ScreenNewOrder({ takeOrder }){
                   </a>
                 </div>
               </div>
-              <section className="w-full h-[calc(100vh-200px)] flex items-center flex-col xl:flex-row gap-1    ">
+              <section className="w-full h-[calc(100vh-145px)] flex items-center flex-col xl:flex-row gap-1    ">
               <section className="bg-slate-900 h-[calc(100vh-400px)] sm:h-[calc(100vh-400px)]   overflow-y-auto w-full  xl:w-1/2  rounded-xl text-sm mt-10 border-separate text-gray-400 ">
                     {
                       takeOrder.line_items.map((product)=>(
@@ -48,6 +48,12 @@ export default function ScreenNewOrder({ takeOrder }){
                       ))
                     }
                     
+                </section>
+                <section className='bg-slate-900 h-[calc(100vh-500px)] flex flex-col w-full xl:w-1/2 rounded-xl    border-separate text-gray-400'>
+               
+                    
+              
+               
                 </section>
               </section>
         </>
