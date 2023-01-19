9ebd237e82f6253a5398822b3e5215fdb581b1cf
Johnny Torres <jony.tz@gmail.com>
2023-01-18 23:48:00 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [takeOrder, setTakeOrder] = useState({ line_items:[] })
   return (
    <div className="bg-gray-900 min-h-screen flex items-start justify-center">
-      <div className="bg-gray-800 h-min flex-1 flex flex-col lg:flex-row space-y-5 lg:space-y-0 lg:space-x-5  max-w-6xl sm:p-6 sm:my-2 sm:mx-4 sm:rounded-2xl">
+      <div className="bg-gray-800  flex-1 flex flex-col lg:flex-row space-y-0 lg:space-y-0 lg:space-x-5  max-w-6xl sm:p-6 sm:my-2 sm:mx-4 sm:rounded-2xl">
         <Nav takeOrder={ takeOrder }/>
         <Screen>
           <Routes>
